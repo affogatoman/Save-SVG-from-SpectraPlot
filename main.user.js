d31ed6302dd52173b412e737e67fdc96f67d2e02
JJH조재희 <colombia2@naver.com>
2020-03-08 16:19:40 +0900

    Update main.user.js

--- main.user.js
+++ main.user.js
@@ -2,7 +2,7 @@
 // @name         Plot To SVG
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @license      Apache 2.0; https://www.apache.org/licenses/LICENSE-2.0
+// @license      MIT
 // @description  Add download SVG button
 // @author       JJH조재희
 // @match        http://www.spectraplot.com/blackbody
